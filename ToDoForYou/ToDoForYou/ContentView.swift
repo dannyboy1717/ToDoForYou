5e7a7bf76e50db4a1923072e25b32034f1122877
Daniel Hughes <dannyboy1717@hotmail.co.uk>
2024-09-17 23:14:02 +0100

    Better view - still learning
    Things I've Learned:
    
    - HStacks, VStacks and ZStacks
    - The order in which you add 'modifiers' to a view matters, e.g. padding before background vs after background

--- ToDoForYou/ToDoForYou/ContentView.swift
+++ ToDoForYou/ToDoForYou/ContentView.swift
@@ -5,14 +5,21 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        VStack {
-            Text("ToDoForYou")
-                .background(in: .capsule, fillStyle: .init(eoFill: true))
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            .foregroundStyle(.indigo)
-            Image("TestBear")
-            Text("Loading...")
-                .font(.subheadline)
+        ZStack {
+            Color.indigo.ignoresSafeArea(.all, edges: .all)
+            
+            VStack {
+                Text("ToDoForYou")
+                    .padding(.all,5)
+                    .background(in: .buttonBorder, fillStyle: .init(eoFill: true))
+                    .frame(width: 200, height: 50, alignment: .top)
+                    .font(.title)
+                    .foregroundStyle(.indigo)
+                    .backgroundStyle(.teal)
+                Image("TestBear").resizable().frame(width: 100, height: 100, alignment: .center)
+                Text("Loading...")
+                    .font(.subheadline)
+            }
         }
     }
     
