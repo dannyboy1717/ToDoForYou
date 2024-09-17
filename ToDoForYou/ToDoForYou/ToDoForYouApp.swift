dcd049c1f6eed863dfcff5f07f36e3bc47674d59
Daniel Hughes <dannyboy1717@hotmail.co.uk>
2024-09-17 12:21:18 +0100

    Initial Xcode project

---
+++ ToDoForYou/ToDoForYou/ToDoForYouApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoForYouApp.swift
+//  ToDoForYou
+//
+//  Created by Daniel Hughes on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoForYouApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
